218f310b2443664ffcbf4e0ae59ca5bff0a20eda
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-05-05 21:16:08 +0300

    Triple Trouble

--- js/codewars0505.js
+++ js/codewars0505.js
@@ -129,3 +129,19 @@ function guessBlue(blueStart, redStart, bluePulled, redPulled) {
     let blueLeft = blueStart-bluePulled;
       return blueLeft/marblesLeft;
     }
+
+//---------------------------------------------------
+// Triple Trouble
+// Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+// E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+// Note: You can expect all of the inputs to be the same length.
+
+function tripleTrouble(one, two, three){
+    let string = '';
+      for (let i=0; i<one.length; i++){
+        string+=one[i]+two[i]+three[i];
+      }
+      return string;
+}
