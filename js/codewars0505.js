36b5604b958c3deb37930d036e769fc7659eb342
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-05-05 21:24:06 +0300

    You only need one - Beginner

--- js/codewars0505.js
+++ js/codewars0505.js
@@ -145,3 +145,14 @@ function tripleTrouble(one, two, three){
       }
       return string;
 }
+
+//-----------------------------------------------------
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+function check(a, x) {
+    return a.indexOf(x) > -1;
+  }
