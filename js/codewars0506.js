a6467a4c4b1b395e6c2f4c95e81ceaf07a01c759
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-05-07 21:18:49 +0300

    how much I love you

--- js/codewars0506.js
+++ js/codewars0506.js
@@ -97,3 +97,27 @@ function fixTheMeerkat(arr) {
 
 //-----------------------------------------
 
+//I love you, a little , a lot, passionately ... not at all
+
+function howMuchILoveYou(nbPetals) {
+    if (nbPetals%6 === 1) {
+      return "I love you";
+    }
+    if (nbPetals%6 === 2) {
+      return "a little";
+    }
+    if (nbPetals%6 === 3) {
+      return "a lot";
+    }
+    if (nbPetals%6 === 4) {
+      return "passionately";
+    }
+    if (nbPetals%6 === 5) {
+      return "madly";
+    } else {
+      return "not at all";
+    }
+}
+// console.log(howMuchILoveYou(7),"I love you");
+// console.log(howMuchILoveYou(3),"a lot");
+// console.log(howMuchILoveYou(6),"not at all");
